e054b59e4d91b4b251902086aa28f33581daa38b
awewsomegamer <awewsomegamer@gmail.com>
2025-10-17 23:19:06 -0500

    PCID: make global
    I have decided to make the PCIDs global. This removes the need to track
    allocated PCIDs for process page tables per processor and allows for
    easier storage (in the page_table pointer).

--- src/c/include/arch/x86-64/pcid.h
+++ src/c/include/arch/x86-64/pcid.h
@@ -1,3 +1,29 @@
+/**
+ * @file pcid.h
+ *
+ * @author awewsomegamer <awewsomegamer@gmail.com>
+ *
+ * @LICENSE
+ * Arctan-OS/Karch-x86-64 - x86-64 Implementation of K/arch Abstractions
+ * Copyright (C) 2023-2025 awewsomegamer
+ *
+ * This file is part of Arctan-OS/Karch-x86-64.
+ *
+ * Arctan-OS/Karch-x86-64 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @DESCRIPTION
+*/
 #ifndef ARC_ARCH_X86_64_PCID_H
 #define ARC_ARCH_X86_64_PCID_H
 
--- src/c/pcid.c
+++ src/c/pcid.c
@@ -1,19 +1,50 @@
+/**
+ * @file pcid.c
+ *
+ * @author awewsomegamer <awewsomegamer@gmail.com>
+ *
+ * @LICENSE
+ * Arctan-OS/Karch-x86-64 - x86-64 Implementation of K/arch Abstractions
+ * Copyright (C) 2023-2025 awewsomegamer
+ *
+ * This file is part of Arctan-OS/Karch-x86-64.
+ *
+ * Arctan-OS/Karch-x86-64 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @DESCRIPTION
+*/
+#include "arch/info.h"
+#include "arch/x86-64/ctrl_regs.h"
 #include "arch/x86-64/pcid.h"
 #include "arch/x86-64/smp.h"
+#include "arch/x86-64/util.h"
 #include "arctan.h"
 #include "global.h"
+#include "lib/spinlock.h"
 #include "mm/allocator.h"
 #include "util.h"
 
-int pcid_allocate() {
-        uint64_t *pcid_bmp = Arc_CurProcessorDescriptor->pcid.bmp;
+static uint64_t *pcid_bmp = NULL;
+static int pcid_last_free = 0;
+static ARC_Spinlock mod_lock;
 
+int pcid_allocate() {
         if (pcid_bmp == NULL) {
                 ARC_DEBUG(ERR, "No PCID bitmap present, cannot allocate\n");
                 return -1;
         }
 
-        int pcid_last_free = Arc_CurProcessorDescriptor->pcid.last_free;
         int pcid = 0;
 
         for (int i = 0; i < PCID_COUNT / (sizeof(*pcid_bmp) * 8); i++) {
@@ -22,6 +53,8 @@ int pcid_allocate() {
                 if (pcid_bmp[_i] == (uint64_t)~0) {
                         continue;
                 }
+
+                spinlock_lock(&mod_lock);
 
                 int x = __builtin_ffs(~pcid_bmp[_i]);
 
@@ -35,23 +68,23 @@ int pcid_allocate() {
                 pcid = (i * sizeof(*pcid_bmp) * 8) + x;
                 pcid_last_free = _i;
 
+                spinlock_unlock(&mod_lock);
+
                 break;
         }
-
-        Arc_CurProcessorDescriptor->pcid.last_free = pcid_last_free;
 
         return pcid;
 }
 
 void pcid_free(int pcid) {
-        uint64_t *pcid_bmp = Arc_CurProcessorDescriptor->pcid.bmp;
-
         if (pcid_bmp == NULL) {
                 ARC_DEBUG(ERR, "No PCID bitmap present, cannot free\n");
                 return;
         }
 
+        spinlock_lock(&mod_lock);
         pcid_bmp[pcid / (sizeof(*pcid_bmp) * 8)] &= ~(1 << (pcid % 64));
+        spinlock_unlock(&mod_lock);
 }
 
 int init_pcid() {
@@ -60,7 +93,17 @@ int init_pcid() {
                 return -1;
         }
 
-        uint64_t *pcid_bmp = alloc(PCID_COUNT / sizeof(*pcid_bmp) * 8);
+        uint64_t cr4 = _x86_getCR4() | 1 << 17; // Set PCIDE bit (17)
+        _x86_setCR4(cr4);
+        ARC_DEBUG(INFO, "Set PCIDe bit\n");
+
+        if (pcid_bmp != NULL) {
+                return 0;
+        }
+
+        init_static_spinlock(&mod_lock);
+
+        pcid_bmp = alloc(PCID_COUNT / sizeof(*pcid_bmp) * 8);
 
         if (pcid_bmp == NULL) {
                 ARC_DEBUG(ERR, "Failed to allocate memory for PCID bmp\n");
@@ -71,8 +114,7 @@ int init_pcid() {
 
         pcid_bmp[0] |= 1;
 
-        Arc_CurProcessorDescriptor->pcid.bmp = pcid_bmp;
-        Arc_CurProcessorDescriptor->pcid.last_free = 0;
+        ARC_DEBUG(INFO, "Initialized PCID allocator\n");
 
         return 0;
 }
