ad5b44e5d4f284d36cc5060342e7a3bef97302cf
awewsomegamer <awewsomegamer@gmail.com>
2026-03-12 00:43:16 -0500

    apic: mask 8259 PIC
    The if statement here was prevent the masking of the 8259 PIC ( which
    is not properly mapped) and caused weird interrupts to be
    triggered. The if statement should be re-introduced and an else
    statement given to it so that if no IO APICS are initialized, the 8259
    PIC still allows for the kernel to service IRQs.

--- src/c/apic.c
+++ src/c/apic.c
@@ -119,11 +119,11 @@ int init_apic() {
 		ioapic_list = next;
 	}
 
-	if (ioapic_list == NULL) {
+        // if (ioapic_list != NULL) {
 		// Disable 8259
 		outb(0x21, 0xFF);
 		outb(0xA1, 0xFF);
-	}
+        // }
 
 	return 0;
 }
